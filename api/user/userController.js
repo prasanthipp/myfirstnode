fceb6dd7e31bc8248fca37738f76c15656b24437
bangi <bangi_111@yahoo.com>
2019-07-13 02:02:14 -0500

    Adding Changes:
    Fixed users/id code:
       This fix should allow all Crud calls on users/id

--- api/user/userController.js
+++ api/user/userController.js
@@ -2,17 +2,20 @@ var User = require('./userModel');
 var _ = require('lodash');
 
 exports.params = function (req, res, next, id) {
+   console.log("DEBUG:: In Id:" + id )
     User.findById(id)
-        .then(function (post) {
+        .then(function (user, err) {
+            console.log("DEBUG:: User:" + user)
+            console.log("DEBUG:: Error:" + err)
+            // !user.name
             if (!user) {
-                next(new Error('No user with that id'));
+                next(new Error('No user with id :' + id));
             } else {
                 req.user = user;
                 next();
             }
         }, function (err) {
             next(err);
-
         });
 }
 
@@ -27,11 +30,13 @@ exports.get = function (req, res, next) {
 };
 
 exports.getOne = function (req, res, next) {
+   console.log("DEBUG:: users getOne ")
     var user = req.user;
     res.json(user);
 };
 
 exports.put = function (req, res, next) {
+   console.log("DEBUG:: users put ")
     var user = req.user;
     var update = req.body;
     _.merge(user, update);
@@ -46,6 +51,7 @@ exports.put = function (req, res, next) {
 };
 
 exports.post = function (req, res, next) {
+    console.log("DEBUG:: users post")
     var newUser = req.body;
     User.create(newUser)
         .then(function (users) {
@@ -56,6 +62,7 @@ exports.post = function (req, res, next) {
 };
 
 exports.delete = function (req, res, next) {
+    console.log("DEBUG:: users delete")
     req.user.remove(function (err, removed) {
         if (err) {
             next(err);
