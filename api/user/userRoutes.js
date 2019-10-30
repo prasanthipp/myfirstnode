09a1822b3d0ab2244beb638e6e33c4898e9dc2fb
PrasanthiPP <prasanti.bangi@gmail.com>
2019-10-30 11:37:32 -0500

    adding changes

--- api/user/userRoutes.js
+++ api/user/userRoutes.js
@@ -5,13 +5,24 @@ router.param('id', controller.params);
 
 router.route('/')
  .get(controller.get)
- .post(controller.post)
-
- router.route('/:id')
+  .post(controller.post)
+ 
+router.route('/:id')
   .get(controller.getOne)
   .put(controller.put)
   .delete(controller.delete)
 
- module.exports=router;
+// router.route('/:id/logout')
+//   .get(controller.logout)
 
+//router.route('/me')
+//  .get(controller.me)
+
+router.route('/login')
+  .post(controller.login)
+
+router.route('/register')
+  .post(controller.register)
+
+module.exports=router;
 
